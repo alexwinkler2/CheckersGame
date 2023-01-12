e171cc278628fc35ae6091bceb5dbec1a816678b
334492832 <334492832@C-4JZC082.tdsb.on.ca>
2023-01-12 14:45:18 -0500

    The board is now an image

--- CheckerGame/src/winkler/alex/GUIDriver.java
+++ CheckerGame/src/winkler/alex/GUIDriver.java
@@ -1,15 +1,22 @@
 package winkler.alex;
 
+import java.io.File;
+import java.io.FileInputStream;
+
 import javafx.application.Application;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -28,26 +35,38 @@ launch(args);
    //hello
     @Override
     public void start(Stage stage) throws Exception {
-    GridPane board = new GridPane();
-   
+    FileInputStream inputstream = new FileInputStream("C:\\Users\\334492832\\Pictures\\checker_board.PNG");
+    Image image = new Image(inputstream);
+    ImageView imageView = new ImageView(image);
+    Group root = new Group ();
+    root.getChildren().addAll(imageView);
+    GridPane checkers= new GridPane();
     //Start Of Squares
     int count = 0;
         for (int i = 0; i < 8; i++) {
         count++;
         for (int j = 0; j < 8; j++) {
-        Rectangle r = new Rectangle(50, 50, 50, 50);
+        Circle c = new Circle();
+        c.setCenterX(200.0f);
+        c.setCenterY(100.0f);
+        c.setRadius(50.0f);
+        c.setFill(Color.BLACK);
         if (count % 2 != 0) {
-        r.setFill(Color.FORESTGREEN);
+        
         }
         else {
-        r.setFill(Color.CORNSILK);
+        
         }
-        board.add(r,j, i);
+        if(i > 0 && i <3) {
+        	
+        }
         count++;
         }
         }
-        Scene scene = new Scene(board);
+        Scene scene = new Scene(root,400,400);
+        
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
 
 
